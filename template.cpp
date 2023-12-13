532974364038a9dab8f676cddcd869a1ca80da4a
WhiteStorm <andrzejmmm1@gmail.com>
2023-12-14 00:56:14 +0100

    Refactor and rename tamplate.cpp to template.cpp
    The commit includes changes to the .gitignore file, adding a new entry for .vscode/ directory. Additionally, the file tamplate.cpp has been renamed to template.cpp with some minor code modifications.

--- template.cpp
+++ template.cpp
@@ -3,23 +3,23 @@
 #include <fstream>
 #include <string>
 
+const std::string DAY = "0";
+
 int main()
 {
     std::fstream input;
     std::string row = "";
     int result = 0;
 
-
-    input.open("3.txt", std::ios::in);
+    input.open(DAY + ".txt", std::ios::in);
     if (input.good())
-        while (!input.eof())
+        while (std::getline(input, row))
         {
-            std::getline(input, row);
-            
         }
     input.close();
 
-    std::cout << std::endl<<result;
+    std::cout << result << std::endl;
 
+    // system("pause");
     return 0;
 }
